65b9582f4362238784bdf6264af9f70f9162cda2
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-11-28 22:52:25 -0800

    Connected all the story together. Need to add a way to restart when getting to the stories with no buttons.

--- Destini/ViewController.swift
+++ Destini/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var storyIndex : Int = 1
 
     // Our strings
     let story1 = "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: \"Need a ride, boy?\"."
@@ -44,13 +46,46 @@ class ViewController: UIViewController {
         
         
         // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton, and to T1_Story, T1_Ans1, and T1_Ans2
+        storyTextView.text = story1
+        topButton.setTitle(answer1a, for: .normal)
+        bottomButton.setTitle(answer1b, for: .normal)
         
     }
 
     
     // User presses one of the buttons
     @IBAction func buttonPressed(_ sender: UIButton) {
-    
+        if sender.tag == 1 && storyIndex == 1 {
+            storyTextView.text = story3
+            topButton.setTitle("\(answer3a)", for: .normal)
+            bottomButton.setTitle("\(answer3b)", for: .normal)
+            storyIndex = 3
+        } else if sender.tag == 2 && storyIndex == 1 {
+            storyTextView.text = story2
+            topButton.setTitle("\(answer2a)", for: .normal)
+            bottomButton.setTitle("\(answer2b)", for: .normal)
+            storyIndex = 2
+        } else if sender.tag == 1 && storyIndex == 3 {
+            storyTextView.text = story6
+            topButton.isHidden = true
+            bottomButton.isHidden = true
+            storyIndex = 6
+        } else if sender.tag == 2 && storyIndex == 3 {
+            storyTextView.text = story5
+            topButton.isHidden = true
+            bottomButton.isHidden = true
+            storyIndex = 5
+        } else if sender.tag == 1 && storyIndex == 2 {
+            storyTextView.text = story3
+            topButton.setTitle("\(answer3a)", for: .normal)
+            bottomButton.setTitle("\(answer3b)", for: .normal)
+            storyIndex = 3
+        } else if sender.tag == 2 && storyIndex == 2 {
+            storyTextView.text = story4
+            topButton.isHidden = true
+            bottomButton.isHidden = true
+            storyIndex = 4
+        }
         // TODO Step 4: Write an IF-Statement to update the views
                 
         // TODO Step 6: Modify the IF-Statement to complete the story
